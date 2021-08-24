09b2f2e7f32f74d70ef739cdce42dc6f06d73b29
Connor Turland <connorturland@gmail.com>
2021-08-24 14:34:23 -0700

    tweak build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,15 @@
 
 # backend
 . scripts/install-hc-tools.sh
+
 . scripts/dna-pack.sh
 
 rm -rf electron/binaries
 mkdir electron/binaries
+
 cp dna/workdir/projects.dna electron/binaries/projects.dna
 cp dna/workdir/profiles.dna electron/binaries/profiles.dna
-
-# DO PLATFORM SPECIFIC BINARIES HERE
+# DO PLATFORM SPECIFIC lair-keystore and holochain-runner BINARIES HERE
 # cp ....../holochain-runner electron/binaries/holochain-runner
 # cp $(which lair-keystore) electron/binaries/lair-keystore
 
