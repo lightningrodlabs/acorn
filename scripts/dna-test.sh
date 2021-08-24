a25ad2499cc3321cdb9d447dcd52a5c8e6165853
Connor Turland <connorturland@gmail.com>
2021-08-24 06:07:38 -0700

    remove dna_help test run

--- scripts/dna-test.sh
+++ scripts/dna-test.sh
@@ -4,5 +4,3 @@ cargo test -j 2 --manifest-path dna/zomes/projects/Cargo.toml --lib --features="
 [ $? -eq 0 ]  || exit 1
 cargo test -j 2 --manifest-path dna/zomes/profiles/Cargo.toml --lib --features="mock" -- --nocapture
 [ $? -eq 0 ]  || exit 1
-cargo test -j 2 --manifest-path dna/crates/dna_help/Cargo.toml --lib --features="mock" -- --nocapture
-[ $? -eq 0 ]  || exit 1
