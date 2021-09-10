8be3353fff6c9f268e4a5bcd2ce941067f6d6b00
Wesley Finck <wesleyfinck@gmail.com>
2021-09-09 17:24:45 -0700

    passing zome init test with realistic mock HDK input and output values

--- dna/tests/src/project/project_init.rs
+++ dna/tests/src/project/project_init.rs
@@ -11,19 +11,25 @@ pub mod tests {
     use projects::project::project_meta::crud::ProjectMeta;
     use projects::project::project_meta::validate::*;
     use projects::init;
+    use projects::project::member::entry::Member;
+    use assert_matches::assert_matches;
 
+    /// Unit testing the `init()` function that is called to ensure an agent can receive signals
+    /// and is listed as a member in the project. There are two main functions called within `init()`: `create_receive_signal_cap_grant()`
+    /// and `join_project_during_init()`. The first function creates a capabilities grant token
+    /// allowing other agents to call a specified zome function (used for sending signals for responsive UI updates)
+    /// and the second function creates a member entry and links it to MEMBER_PATH 
     #[test]
     fn test_init() {
                
         let mut mock_hdk = MockHdkT::new();
         
         let zome_info = fixt!(ZomeInfo);
-        // do these mock functions need to be called in the same order as the actual code?
         // init calls create_receive_signal_cap_grant, which calls zome_info and create_cap_grant
         mock_hdk
             .expect_zome_info()
             .times(1)
-            .return_const(Ok(zome_info.clone())); // define what it should return when it matches the `with` statement, or matches at all (without `.with`)
+            .return_const(Ok(zome_info.clone())); 
 
         // create_cap_grant calls just `create` under the hood
         let mut functions: GrantedFunctions = BTreeSet::new();
@@ -45,43 +51,71 @@ pub mod tests {
             .return_const(Ok(header_hash.clone()));
         
         // init also calls join_project_during_init, drilling down, it calls the following hdk functions under the hood
-        // TODO: handle cases where a path with 1 or more parents doesn't yet exist in the DHT (this would call `create` and `create_link` additional times)
-        // 1. `Path::from(MEMBER_PATH).ensure()?;`
-        // 2. `let member_path_address = Path::from(MEMBER_PATH).hash()?;`
-        // 3. `let member_entry_hash = hash_entry(&member)?;`
-        let entry_hash = fixt!(EntryHash);
+        // TODO: test when the MEMBER_PATH doesn't exist in DHT
+        let member_path = Path::from("member");
+        let member_path_entry = Entry::try_from(member_path).unwrap();
+        let member_path_entry_hash = fixt!(EntryHash);
         mock_hdk
-            .expect_hash_entry()
-            .times(3)
-            .return_const(Ok(entry_hash.clone()));
-
-        // relates to `Path::from(MEMBER_PATH).ensure()?;`
-        let expected_output = fixt!(Element);
-        let exp_vec = vec![Some(expected_output)];
-        mock_hdk
-            .expect_get()
+            .expect_hash_entry() // called from `Path::from(MEMBER_PATH).ensure()?;`
+            .with(mockall::predicate::eq(member_path_entry.clone()))
             .times(1)
-            .return_const(ExternResult::Ok(exp_vec));
+            .return_const(Ok(member_path_entry_hash.clone()));
 
+
+        let member_path_get_input = vec![GetInput::new(
+            AnyDhtHash::from(member_path_entry_hash.clone()), GetOptions::content()
+        )];
+        // assuming the path exists on DHT
+        let expected_get_output = vec![Some(fixt!(Element))];
+        mock_hdk
+            .expect_get() // called from `Path::from(MEMBER_PATH).ensure()?;`
+            .with(mockall::predicate::eq(member_path_get_input))
+            .times(1)
+            .return_const(Ok(expected_get_output));
+
+        mock_hdk
+            .expect_hash_entry() // called from `let member_path_address = Path::from(MEMBER_PATH).hash()?;`
+            .with(mockall::predicate::eq(member_path_entry.clone()))
+            .times(1)
+            .return_const(Ok(member_path_entry_hash.clone()));
+        
         let agent_info = fixt!(AgentInfo);
         mock_hdk
             .expect_agent_info()
             .times(1)
             .return_const(Ok(agent_info.clone()));
         
+        let member = Member {
+            address: WrappedAgentPubKey(agent_info.agent_initial_pubkey),
+        };
+        let create_member_input = EntryWithDefId::try_from(member.clone()).unwrap();
         mock_hdk
             .expect_create()
+            .with(mockall::predicate::eq(create_member_input))
             .times(1)
             .return_const(Ok(header_hash.clone()));
         
+        let member_hash = fixt!(EntryHash);
+        let member_entry = Entry::try_from(member.clone()).unwrap();
+        mock_hdk
+            .expect_hash_entry() // called from `let member_entry_hash = hash_entry(&member)?;`
+            .with(mockall::predicate::eq(member_entry))
+            .times(1)
+            .return_const(Ok(member_hash.clone()));
+        let create_link_input = CreateLinkInput::new(
+            member_path_entry_hash,
+            member_hash,
+            LinkTag::from(()),
+        );
         mock_hdk
             .expect_create_link()
+            .with(mockall::predicate::eq(create_link_input))
             .times(1)
             .return_const(Ok(header_hash));
 
         set_hdk(mock_hdk);
 
         let result = init(());
-        assert_eq!(result.is_ok(), true);
+        assert_matches!(result, Ok(InitCallbackResult::Pass));
     }
 }
