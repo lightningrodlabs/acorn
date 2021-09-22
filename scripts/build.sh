3e9b25d179105f48a158f23e2b93ffbdce2cd460
Wesley Finck <wesleyfinck@gmail.com>
2021-09-22 14:04:26 -0700

    skipping holochain builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # backend
-. scripts/install-hc-tools.sh
+# . scripts/install-hc-tools.sh
 
-. scripts/dna-pack.sh
+# . scripts/dna-pack.sh
 
 rm -rf electron/binaries
 mkdir electron/binaries
 
-cp dna/workdir/projects.dna electron/binaries/projects.dna
-cp dna/workdir/profiles.dna electron/binaries/profiles.dna
+# cp dna/workdir/projects.dna electron/binaries/projects.dna
+# cp dna/workdir/profiles.dna electron/binaries/profiles.dna
 
 # DO PLATFORM SPECIFIC lair-keystore and holochain-runner BINARIES HERE
 # . scripts/copy-binaries
