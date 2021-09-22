d35d6b4ee60d1637e1fd84d59c2ca07a86733e96
Connor Turland <connorturland@gmail.com>
2021-09-22 16:51:16 -0700

    make comments

--- scripts/install-hc-tools.sh
+++ scripts/install-hc-tools.sh
@@ -4,4 +4,7 @@
 rustup target install wasm32-unknown-unknown
 
 # install `hc` cli tool
+# use the version from August 18, 2021 to match version
+# 0.0.103 of holochain + hdk
+# KEEP THIS IN SYNC
 cargo install holochain_cli --version 0.0.4
