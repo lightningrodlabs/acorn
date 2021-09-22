d3a7fd8b7b1f5b9204c7ea38ac81ed741d41b718
Wesley Finck <wesleyfinck@gmail.com>
2021-09-22 14:52:48 -0700

    test full build on mac and linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # backend
-# . scripts/install-hc-tools.sh
+. scripts/install-hc-tools.sh
 
-# . scripts/dna-pack.sh
+. scripts/dna-pack.sh
 
 rm -rf electron/binaries
 mkdir electron/binaries
 
-# cp dna/workdir/projects.dna electron/binaries/projects.dna
-# cp dna/workdir/profiles.dna electron/binaries/profiles.dna
+# ensure all necessary binaries are packaged in the app
+cp dna/workdir/projects.dna electron/binaries/projects.dna
+cp dna/workdir/profiles.dna electron/binaries/profiles.dna
 
 # DO PLATFORM SPECIFIC lair-keystore and holochain-runner BINARIES HERE
-# . scripts/copy-binaries
+. scripts/copy-binaries
 
 # ui
 rm -rf electron/web
