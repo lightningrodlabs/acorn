528a38e238daccea1c24df26c4093d98e4b535a3
Connor Turland <connorturland@gmail.com>
2021-09-22 09:58:18 -0700

    fix ups for building for now

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,12 +12,13 @@ cp dna/workdir/projects.dna electron/binaries/projects.dna
 cp dna/workdir/profiles.dna electron/binaries/profiles.dna
 
 # DO PLATFORM SPECIFIC lair-keystore and holochain-runner BINARIES HERE
-. scripts/copy-binaries
+# . scripts/copy-binaries
 
 # ui
 rm -rf electron/web
 npm run web-build
 cp -r web/dist electron/web
+
 cd electron
 npm run build
 
