a4022865af285aad0761dce1ac000cce56627f05
Wesley Finck <wesleyfinck@gmail.com>
2021-09-22 15:32:26 -0700

    fixed file path to copy-binaries.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,7 +14,7 @@ cp dna/workdir/projects.dna electron/binaries/projects.dna
 cp dna/workdir/profiles.dna electron/binaries/profiles.dna
 
 # DO PLATFORM SPECIFIC lair-keystore and holochain-runner BINARIES HERE
-. scripts/copy-binaries
+. scripts/copy-binaries.sh
 
 # ui
 rm -rf electron/web
