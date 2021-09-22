432fc2943d6165a4a486a818b8e91fd971d91a97
Connor Turland <connorturland@gmail.com>
2021-09-22 16:48:00 -0700

    use a version of hc to match 0.0.103 holochain to add windows support for lair-keystore

--- scripts/install-hc-tools.sh
+++ scripts/install-hc-tools.sh
@@ -4,6 +4,4 @@
 rustup target install wasm32-unknown-unknown
 
 # install `hc` cli tool
-cargo install holochain_cli \
-  --git https://github.com/holochain/holochain.git \
-  --rev a6ac0439670ba367c723a80d3b8bc7c419aa5f6e
+cargo install holochain_cli --version 0.0.4
