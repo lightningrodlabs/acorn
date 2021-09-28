68ad68bdb48aafc278a365dcd2ee6f36ac0c2382
Connor Turland <connorturland@gmail.com>
2021-09-28 14:39:53 -0700

    separate dev and prod because they should be

--- web/webpack.dev.js
+++ web/webpack.dev.js
@@ -1,18 +1,14 @@
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const ReactRefreshTypeScript = require('react-refresh-typescript')
 const fs = require('fs')
 const path = require('path')
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
 const webpack = require('webpack')
 const mainAppId = fs.readFileSync(path.join(__dirname, '../config-main-app-id'), 'utf-8')
 
-module.exports = merge(common, {
+// const isDevelopment = true
+module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true // hot module reloading
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new ReactRefreshWebpackPlugin(),
@@ -22,4 +18,79 @@ module.exports = merge(common, {
       __APP_PORT__: process.env.APP_WS_PORT,
     }),
   ],
-})
+  entry: {
+    app: './src/index.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    fs: 'empty',
+  },
+  devServer: {
+    host: 'localhost',
+    disableHostCheck: true,
+    contentBase: './dist',
+    hot: true // hot module reloading
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+            plugins: [
+              // ... other plugins
+              require.resolve('react-refresh/babel'),
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+            options: {
+              getCustomTransformers: () => ({
+                before: [ReactRefreshTypeScript()],
+              }),
+              // `ts-loader` does not work with HMR unless `transpileOnly` is used.
+              // If you need type checking, `ForkTsCheckerWebpackPlugin` is an alternative.
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: (url, resourcePath) => {
+                // resourcePath - path to css file
+
+                // Don't handle `splash-image.png` urls
+                if (url.includes('splash-image.png')) {
+                  return false
+                }
+
+                return true
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
--- web/webpack.prod.js
+++ web/webpack.prod.js
@@ -1,14 +1,17 @@
 const fs = require('fs')
 const path = require('path')
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
 const webpack = require('webpack')
-const mainAppId = fs.readFileSync(path.join(__dirname, '../config-main-app-id'), 'utf-8')
+const mainAppId = fs.readFileSync(
+  path.join(__dirname, '../config-main-app-id'),
+  'utf-8'
+)
 
-module.exports = merge(common, {
+module.exports = {
   mode: 'production',
   output: {
     publicPath: './',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'source-map',
   plugins: [
@@ -18,4 +21,56 @@ module.exports = merge(common, {
       __APP_PORT__: 8889,
     }),
   ],
-})
+  entry: {
+    app: './src/index.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  node: {
+    fs: 'empty',
+  },
+  devServer: {
+    host: 'localhost',
+    disableHostCheck: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+            plugins: [],
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: (url, resourcePath) => {
+                // resourcePath - path to css file
+                // Don't handle `splash-image.png` urls
+                if (url.includes('splash-image.png')) {
+                  return false
+                }
+                return true
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
