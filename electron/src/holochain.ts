5d9dcf71ea7f82fad07bcaa304e07ec823496335
Wesley Finck <wesleyfinck@gmail.com>
2021-10-06 17:19:48 -0700

    Update holochain.ts

--- electron/src/holochain.ts
+++ electron/src/holochain.ts
@@ -76,11 +76,11 @@ const devOptions: HolochainRunnerOptions = {
 }
 const prodOptions: HolochainRunnerOptions = {
   dnaPath: profilesDnaPath, // preload
-  datastorePath: path.join(app.getPath('userData'), 'databases-0-5-4-test-1'),
+  datastorePath: path.join(app.getPath('userData'), 'databases-0-5-4'),
   appId: MAIN_APP_ID,
   appWsPort: 8889,
   adminWsPort: 1235,
-  keystorePath: path.join(app.getPath('userData'), 'keystore-0-5-4-test-1'),
+  keystorePath: path.join(app.getPath('userData'), 'keystore-0-5-4'),
   proxyUrl: COMMUNITY_PROXY_URL,
 }
 
