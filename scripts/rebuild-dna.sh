c9c490c983e9dba99232eae3324cf30e06049d8a
Wesley Finck <wesleyfinck@gmail.com>
2022-01-06 21:04:04 -0800

    removed unused scripts

--- scripts/rebuild-dna.sh
+++
@@ -1,8 +0,0 @@
-
-# will clear existing session, beware!
-npm run dna-pack
-cp dna/workdir/profiles.dna electron/binaries/profiles.dna
-cp dna/workdir/projects.dna electron/binaries/projects.dna
-rm -rf user-data
-rm -rf user2-data
-# npm run electron
