a5ba528749ee752885fbc529c69ebde85baca606
Wesley Finck <wesleyfinck@gmail.com>
2022-01-16 17:21:52 -0800

    middleware sends realtime info signal when open and close expanded view

--- web/src/index.js
+++ web/src/index.js
@@ -22,6 +22,7 @@ import acorn from './reducer'
 import signalsHandlers from './signalsHandlers'
 import { setProfilesCellId, setProjectsCellIds } from './cells/actions'
 import { layoutWatcher } from './layout/middleware'
+import { realtimeInfoWatcher } from './realtime-info-signal/middleware'
 import { fetchAgents } from './agents/actions'
 import { whoami } from './who-am-i/actions'
 import { fetchAgentAddress } from './agent-address/actions'
@@ -37,7 +38,7 @@ import { getProjectCellIdStrings } from './projectAppIds'
 // trigger caching of adminWs connection
 getAdminWs()
 
-const middleware = [holochainMiddleware(APP_WS_URL), layoutWatcher]
+const middleware = [holochainMiddleware(APP_WS_URL), layoutWatcher, realtimeInfoWatcher]
 
 // This enables the redux-devtools browser extension
 // which gives really awesome debugging for apps that use redux
--- web/src/realtime-info-signal/middleware.js
+++ web/src/realtime-info-signal/middleware.js
@@ -1,116 +1,53 @@
-import TWEEN from '@tweenjs/tween.js'
 import {
-    createGoalWithEdge,
-    fetchGoals,
-    archiveGoalFully
-} from '../projects/goals/actions'
-import {
-    createEdge,
-    fetchEdges,
-    AFFECT_LAYOUT_ARCHIVE_EDGE,
-    PREVIEW_EDGES,
-    CLEAR_EDGES_PREVIEW,
-} from '../projects/edges/actions'
-import {
-  TRIGGER_UPDATE_LAYOUT,
-} from '../layout/actions'
-import panZoomToFrame from '../animations/pan-and-zoom'
-import { ANIMATE_PAN_AND_ZOOM } from '../viewport/actions'
-import performLayoutAnimation from '../animations/layout'
+  OPEN_EXPANDED_VIEW,
+  CLOSE_EXPANDED_VIEW,
+} from '../expanded-view/actions'
+import { sendRealtimeInfoSignal } from './actions'
+
+
 
 // capture open/close expanded view, start/end title/description edit, and enter/exit project actions
 // import action constants
 // isOneOfRealtimeInfoAffectingActions checks action type against
+// can first start with just open close expanded view, then change the title/description edits action logic (and state) then implement in middleware
 
-
-const isOneOfLayoutAffectingActions = (action) => {
-    const { type } = action
-    return type === TRIGGER_UPDATE_LAYOUT
-        || type === PREVIEW_EDGES
-        || type === CLEAR_EDGES_PREVIEW
-        || type === createGoalWithEdge.success().type
-        || type === fetchGoals.success().type
-        || type === archiveGoalFully.success().type
-        || type === createEdge.success().type
-        || type === fetchEdges.success().type
-        || (action.type === AFFECT_LAYOUT_ARCHIVE_EDGE && action.affectLayout)
-}
-
-const isOneOfViewportAffectingActions = (action) => {
+const isOneOfRealtimeInfoAffectingActions = (action) => {
   const { type } = action
-  return type === ANIMATE_PAN_AND_ZOOM
+  return type === OPEN_EXPANDED_VIEW
+      || type === CLOSE_EXPANDED_VIEW
 }
+// watch for actions that will affect the realtime info state values
 
-// watch for actions that will affect the layout (create/fetches/deletes)
-// and update the layout when we see an action like that
-// it is useful to have this layout cached in the state for performance reasons
+const realtimeInfoWatcher = store => {
 
-const layoutWatcher = store => {
-
-  // when this middleware is called
-  // run an animation loop which will performantly handle 
-  // the animation of layouts
-  animate()
-  function animate() {
-    requestAnimationFrame(animate)
-    TWEEN.update()
-  }
 
   // return the action handler middleware
   return next => async action => {
 
-      // in many cases, we just skip right over this middleware, 
-      // and it has no effect. it is only during 'layout affecting actions'
-      // (anything that deletes or adds Goals or Edges which form the graph we
-      // pass to dagre to generate a layout) that this kicks in, and handles the creation of an animation
-      // from the current layout to the new layout, by using the TWEENJS library
-
-      // catch and handle this uniquely special action
-      // which has a special case of being a delete firing very close in time to
-      // a create action (since we delete an Edge then immediately create one)
-      // and allow preventing that action in special cases from causing a layout animation
-      // to occur
-      const specialLayoutAffectingArchiveEdge = action.type === AFFECT_LAYOUT_ARCHIVE_EDGE
-      if (specialLayoutAffectingArchiveEdge) {
-        if (!action.affectLayout) {
-          // just dispatch the "real" action and return that
-          return store.dispatch(action.asyncAction)
-        } else {
-          // wait for the async action to complete before running re-layout
-          await store.dispatch(action.asyncAction)
-          // now the store will have the edge removed from state
-          // meaning the end calculated layout will be proper
-        }
-      }
-
-      let currentState
-      const shouldReLayout = isOneOfLayoutAffectingActions(action)
-      const shouldAnimateViewport = isOneOfViewportAffectingActions(action)
-      // don't call and getState if we don't have to
-      if (shouldReLayout || shouldAnimateViewport) {
-        currentState = store.getState()
-      }
-      // perform the usual (next) action ->
-      // first integrate the new data
-      // that way the layout recalculation
-      // will include it
+    const shouldSendRealtimeSignal = isOneOfRealtimeInfoAffectingActions(action)
+    if (shouldSendRealtimeSignal) {
+      console.log('logging middleware:', action.payload) 
       let result = next(action)
-
-      // in the case of a special "layout affecting"
-      // action, we should also recalculate the layout
-      // based on the new graph of Goals and Edges
-      // and animate to it
-      if (shouldReLayout) {
-        performLayoutAnimation(store, action, currentState)
-      } else if (shouldAnimateViewport) {
-        panZoomToFrame(store, action, currentState)
-      }
-
+      let state = store.getState()
+      store.dispatch(sendRealtimeInfoSignal.create(getRealtimeInfo(state)))
       return result
+    }
+    // return result
+    let result = next(action)
+    return result
   }
 }
 
-
+// eventually, this function would also be used when sending real time info at set interval
+function getRealtimeInfo(state) {
+  let cellIdString = state.ui.activeProject
+  let payload = {
+    project_id: state.ui.activeProject,
+    goal_being_edited: null, //get this from state, once update that part
+    goal_expanded_view: state.ui.expandedView.goalAddress
+  }
+  return { cellIdString, payload }
+}
 export {
-    layoutWatcher
+    realtimeInfoWatcher
 }
