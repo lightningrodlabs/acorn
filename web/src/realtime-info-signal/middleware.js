67cf1cefe21ce234c6b42b20b77a41eff0f3a475
Wesley Finck <wesleyfinck@gmail.com>
2022-01-25 12:55:14 -0800

    cleaned up middleware.js and removed some comments

--- web/src/realtime-info-signal/middleware.js
+++ web/src/realtime-info-signal/middleware.js
@@ -8,24 +8,23 @@ import {
   START_DESCRIPTION_EDIT,
   END_DESCRIPTION_EDIT,
 } from '../goal-editing/actions'
-import { sendRealtimeInfoSignal, SEND_REALTIME_INFO, SEND_EXIT_PROJECT_SIGNAL } from './actions'
-
-
-
-// capture open/close expanded view, start/end title/description edit, and enter/exit project actions
-// import action constants
-// isOneOfRealtimeInfoAffectingActions checks action type against
-// can first start with just open close expanded view, then change the title/description edits action logic (and state) then implement in middleware
+import {
+  sendRealtimeInfoSignal,
+  SEND_REALTIME_INFO,
+  SEND_EXIT_PROJECT_SIGNAL,
+} from './actions'
 
 const isOneOfRealtimeInfoAffectingActions = (action) => {
   const { type } = action
-  return type === OPEN_EXPANDED_VIEW
-      || type === CLOSE_EXPANDED_VIEW
-      || type === START_TITLE_EDIT
-      || type === END_TITLE_EDIT
-      || type === START_DESCRIPTION_EDIT
-      || type === END_DESCRIPTION_EDIT
-      || type === SEND_REALTIME_INFO
+  return (
+    type === OPEN_EXPANDED_VIEW ||
+    type === CLOSE_EXPANDED_VIEW ||
+    type === START_TITLE_EDIT ||
+    type === END_TITLE_EDIT ||
+    type === START_DESCRIPTION_EDIT ||
+    type === END_DESCRIPTION_EDIT ||
+    type === SEND_REALTIME_INFO
+  )
 }
 
 const isProjectExitAction = (action) => {
@@ -34,12 +33,9 @@ const isProjectExitAction = (action) => {
 }
 // watch for actions that will affect the realtime info state values
 
-const realtimeInfoWatcher = store => {
-
-
+const realtimeInfoWatcher = (store) => {
   // return the action handler middleware
-  return next => async action => {
-
+  return (next) => async (action) => {
     const shouldSendRealtimeSignal = isOneOfRealtimeInfoAffectingActions(action)
     if (shouldSendRealtimeSignal) {
       let result = next(action)
@@ -65,26 +61,13 @@ const realtimeInfoWatcher = store => {
   }
 }
 // cherry-pick the relevant state for sending as a RealtimeInfo Signal
-// eventually, this function would also be used when sending real time info at set interval
 function getRealtimeInfo(state) {
   let cellIdString = state.ui.activeProject
-  // let goalBeingEdited
-  // if ("goalAddress" in state.ui.goalEditing && "isTitle" in state.ui.goalEditing) {
-  //   goalBeingEdited = {
-  //     goalAddress: state.ui.goalEditing.goalAddress,
-  //     isTitle: state.ui.goalEditing.isTitle,
-  //   }
-  // }
-  // else {
-  //   goalBeingEdited = null
-  // }
   let payload = {
     projectId: state.ui.activeProject,
     goalBeingEdited: state.ui.goalEditing,
-    goalExpandedView: state.ui.expandedView.goalAddress
+    goalExpandedView: state.ui.expandedView.goalAddress,
   }
   return { cellIdString, payload }
 }
-export {
-    realtimeInfoWatcher
-}
+export { realtimeInfoWatcher }
