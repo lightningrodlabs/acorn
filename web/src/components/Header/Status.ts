cda3e8ab3d4349ec4fd5357ffabac0d94ea9286f
Connor Turland <connorturland@gmail.com>
2022-02-02 20:54:06 -0800

    remove unnecessary commented out code

--- web/src/components/Header/Status.ts
+++ web/src/components/Header/Status.ts
@@ -4,12 +4,6 @@ enum Status {
   Offline = 'Offline',
 }
 
-// const StatusIcons = {
-//   [Status.Online]: 'checkmark-circle.svg',
-//   [Status.Away]: 'user-status-away.svg',
-//   [Status.Offline]: 'user-status-offline.svg',
-// }
-
 const StatusCssColorClass = {
   [Status.Online]: 'status-online',
   [Status.Away]: 'status-away',
