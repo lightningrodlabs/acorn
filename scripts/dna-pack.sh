3926cf6d80a2dab9f10550979c32ba59188fb358
Wesley Finck <wesleyfinck@gmail.com>
2022-03-04 12:00:28 -0800

    updated electron-holochain to newest version

--- scripts/dna-pack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Compile the WASM
-cargo build --release --target wasm32-unknown-unknown --manifest-path dna/zomes/projects/Cargo.toml
-cargo build --release --target wasm32-unknown-unknown --manifest-path dna/zomes/profiles/Cargo.toml
-# Pack DNAs
-hc dna pack --output=dna/workdir/profiles.dna dna/workdir/dna/profiles
-hc dna pack --output=dna/workdir/projects.dna dna/workdir/dna/projects
