cc95d0c4fb373299dbe37c7f79753c436cf10b15
Wesley Finck <wesleyfinck@gmail.com>
2022-03-04 14:50:55 -0800

    fixed path name

--- scripts/happ-test.sh
+++ scripts/happ-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # --nocapture makes sure the logging output is visible
-cargo test -j 2 --manifest-path dna/tests/Cargo.toml --lib --features="mock" -- --nocapture
+cargo test -j 2 --manifest-path happ/tests/Cargo.toml --lib --features="mock" -- --nocapture
 [ $? -eq 0 ]  || exit 1
