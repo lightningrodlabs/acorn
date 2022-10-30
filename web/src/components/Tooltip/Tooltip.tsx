2dc4bf2a4a153487611f7c17d53be8ec2357e518
Connor Turland <connor@sprillow.com>
2022-10-30 13:33:44 -0700

    move tooltip scss to own file

--- web/src/components/Tooltip/Tooltip.tsx
+++ web/src/components/Tooltip/Tooltip.tsx
@@ -4,20 +4,22 @@ import React from 'react'
 import TooltipTriangleOnTop from '../../images/tooltip-triangle-top.svg'
 // @ts-ignore
 import TooltipTriangleOnBottom from '../../images/tooltip-triangle-bottom.svg'
+import './Tooltip.scss'
 
 export type TooltipProps = {
   text: string
   top?: boolean
+  noTriangle?: boolean
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ text, top }) => {
+const Tooltip: React.FC<TooltipProps> = ({ text, top, noTriangle }) => {
   return (
     <div className={`tooltip-wrapper ${top ? 'top' : ''}`}>
-      {!top && (
+      {!top && !noTriangle && (
         <img className="tooltip-triangle-on-top" src={TooltipTriangleOnTop} />
       )}
       <div className="tooltip-text-wrapper">{`${text}`}</div>
-      {top && (
+      {top && !noTriangle && (
         <img
           className="tooltip-triangle-on-bottom"
           src={TooltipTriangleOnBottom}
