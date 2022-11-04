a8e53ebb5d426ddcd7caddce39034dcc961905ec
Wesley Finck <wesleyfinck@gmail.com>
2022-11-04 12:30:01 -0700

    add function to convert byte array to string

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,6 +1,7 @@
 import { HoloHash, CellId } from '@holochain/client'
 import BufferAll from 'buffer/'
 const Buffer = BufferAll.Buffer
+import { Base64 } from "js-base64"
 
 export function hashToString(hash: HoloHash) {
   // nodejs
@@ -35,4 +36,7 @@ export function cellIdFromString(str: string): CellId {
   // [DnaHash, AgentPubKey]
   const [dnahashstring, agentpubkeyhashstring] = str.split(CELL_ID_DIVIDER)
   return [hashFromString(dnahashstring), hashFromString(agentpubkeyhashstring)]
+}
+export function serializeHash(hash: Uint8Array): string {
+  return `u${Base64.fromUint8Array(hash, true)}`
 }
