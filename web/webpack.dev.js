597fa534c53f7d2c3c00635f39a89808b3def8a2
Karl Bonnici <karl.bonnici100@gmail.com>
2023-04-18 13:51:00 -0700

    add port environment variable to webpack devServer

--- web/webpack.dev.js
+++ web/webpack.dev.js
@@ -47,6 +47,7 @@ module.exports = {
   },
   devServer: {
     host: '0.0.0.0',
+    port: process.env.PORT || 8080,
     allowedHosts: 'all',
     static: './dist',
     hot: true, // hot module reloading
