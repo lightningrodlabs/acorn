f5a53ce428c2bddb4838c10909d64fd7eb5f1117
Karl Bonnici <karl.bonnici100@gmail.com>
2023-04-19 11:07:34 -0700

    stop connection from being deleted on mouse up

--- web/src/redux/ephemeral/outcome-connector/handler.js
+++ web/src/redux/ephemeral/outcome-connector/handler.js
@@ -1,5 +1,8 @@
 import { RELATION_AS_PARENT, resetOutcomeConnector } from './actions'
-import { createConnection, deleteConnection } from '../../persistent/projects/connections/actions'
+import {
+  createConnection,
+  deleteConnection,
+} from '../../persistent/projects/connections/actions'
 import ProjectsZomeApi from '../../../api/projectsApi'
 import { getAppWs } from '../../../hcWebsockets'
 import { cellIdFromString } from '../../../utils'
@@ -17,26 +20,17 @@ export default async function handleConnectionConnectMouseUp(
   const appWebsocket = await getAppWs()
   const projectsZomeApi = new ProjectsZomeApi(appWebsocket)
   if (fromAddress && toAddress) {
-    // if we are replacing an connection with this one
-    // delete the existing connection first
-    // ASSUMPTION: one parent
-    if (existingParentConnectionAddress) {
-      // don't trigger TRIGGER_LAYOUT_UPDATE here
-      // because we will only be archiving
-      // an connection here in the context of immediately replacing
-      // it with another
-      await projectsZomeApi.connection.delete(cellId, existingParentConnectionAddress)
-      dispatch(deleteConnection(activeProject, existingParentConnectionAddress))
-    }
-
     const fromAsParent = relation === RELATION_AS_PARENT
     const createdConnection = await projectsZomeApi.connection.create(cellId, {
-        parentActionHash: fromAsParent ? fromAddress : toAddress,
-        childActionHash: fromAsParent ? toAddress : fromAddress,
-        randomizer: Date.now(),
-        isImported: false
-      })
-    const createConnectionAction = createConnection(activeProject, createdConnection)
+      parentActionHash: fromAsParent ? fromAddress : toAddress,
+      childActionHash: fromAsParent ? toAddress : fromAddress,
+      randomizer: Date.now(),
+      isImported: false,
+    })
+    const createConnectionAction = createConnection(
+      activeProject,
+      createdConnection
+    )
     dispatch(createConnectionAction)
   }
   dispatch(resetOutcomeConnector())
