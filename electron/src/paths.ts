755b50a9c8c51b24fb94209c4e4dab449b3c16c1
Karl Bonnici <karl.bonnici100@gmail.com>
2023-04-28 12:36:42 -0700

    increment version numbers for release

--- electron/src/paths.ts
+++ electron/src/paths.ts
@@ -3,12 +3,14 @@ import * as path from 'path'
 
 // see the DEVELOPERS.md about incrementing
 // these values
-export const INTEGRITY_VERSION_NUMBER = 7
-export const KEYSTORE_VERSION_NUMBER = 3
+export const INTEGRITY_VERSION_NUMBER = 8
+export const KEYSTORE_VERSION_NUMBER = 4
 
-const userNumber = process.env.ACORN_TEST_USER_2 ? "2" : ""
+const userNumber = process.env.ACORN_TEST_USER_2 ? '2' : ''
 const MIGRATION_FILE_NAME_PREFIX = `data-migration-for-integrity-version-`
-const USER_DATA_PATH = app.isPackaged ? app.getPath('userData') : path.join(__dirname, `../../user${userNumber}-data/`)
+const USER_DATA_PATH = app.isPackaged
+  ? app.getPath('userData')
+  : path.join(__dirname, `../../user${userNumber}-data/`)
 const USER_DATA_MIGRATION_FILE_PATH = path.join(
   USER_DATA_PATH,
   `${MIGRATION_FILE_NAME_PREFIX}${INTEGRITY_VERSION_NUMBER}`
@@ -18,4 +20,8 @@ const PREV_VER_USER_DATA_MIGRATION_FILE_PATH = path.join(
   USER_DATA_PATH,
   `${MIGRATION_FILE_NAME_PREFIX}${prevIntegrityVersion}`
 )
-export { USER_DATA_PATH, USER_DATA_MIGRATION_FILE_PATH, PREV_VER_USER_DATA_MIGRATION_FILE_PATH }
+export {
+  USER_DATA_PATH,
+  USER_DATA_MIGRATION_FILE_PATH,
+  PREV_VER_USER_DATA_MIGRATION_FILE_PATH,
+}
