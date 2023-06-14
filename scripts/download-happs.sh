2cd5d69f0102b0476829b2e49f5c93d3d7127457
Connor Turland <connorturland@gmail.com>
2023-06-14 16:37:47 -0700

    try ssl disable againg

--- scripts/download-happs.sh
+++ scripts/download-happs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # bump this in tandem with bumping the INTEGRITY_VERSION_NUMBER
-curl -sL --ssl-no-revoke https://github.com/lightningrodlabs/acorn-happ/releases/download/v6.0.0/profiles.happ -o electron/binaries/profiles.happ
-curl -sL --ssl-no-revoke https://github.com/lightningrodlabs/acorn-happ/releases/download/v6.0.0/projects.happ -o electron/binaries/projects.happ
+curl -sL --insecure https://github.com/lightningrodlabs/acorn-happ/releases/download/v6.0.0/profiles.happ -o electron/binaries/profiles.happ
+curl -sL --insecure https://github.com/lightningrodlabs/acorn-happ/releases/download/v6.0.0/projects.happ -o electron/binaries/projects.happ
