e771d79c603c6addd76e1eaa0448990bfd6b9318
Karl Bonnici <karl.bonnici100@gmail.com>
2023-07-13 13:02:51 -0700

    mock whoami

--- web/test/export.test.ts
+++ web/test/export.test.ts
@@ -1,9 +1,45 @@
-import { useStore } from 'react-redux'
-import exportProjectsData from '../src/migrating/export'
+import { WireRecord } from '../src/api/hdkCrud'
+import { internalExportProjectsData } from '../src/migrating/export'
+import { Profile } from '../src/types'
 
 describe('test export functionality', () => {
+  let whoami: WireRecord<Profile>
+
+  beforeEach(() => {
+    whoami = {
+      actionHash: 'testActionHash',
+      entryHash: 'testEntryHash',
+      createdAt: 1234, // nanoseconds
+      updatedAt: 1234, // nanoseconds
+      entry: {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        handle: 'testHandle',
+        status: 'Online',
+        avatarUrl: 'testAvatarUrl',
+        agentPubKey: 'testAgentPubKey',
+        isImported: false,
+      },
+    }
+  })
   it('should return projects data', async () => {
-    const result = await exportProjectsData(useStore(), 'test', (_, __) => {})
-    console.log(result)
+    const constructProjectDataFetchers = jest.fn()
+    const collectExportProjectData = jest.fn()
+    const store = {
+      dispatch: jest.fn(),
+      getState: jest.fn().mockReturnValue({ whoami }),
+    }
+    const toVersion = 'test'
+    const onStep = jest.fn()
+
+    const result = await internalExportProjectsData(
+      constructProjectDataFetchers,
+      collectExportProjectData,
+      store,
+      toVersion,
+      onStep
+    )
+
+    expect(onStep).toHaveBeenCalled()
   })
 })
