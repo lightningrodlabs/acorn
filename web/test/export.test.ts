2b7df95e9265b0cb62babe075384221247800f90
Karl Bonnici <karl.bonnici100@gmail.com>
2023-07-24 12:17:45 -0700

    add tests for exportProjectsData

--- web/test/export.test.ts
+++ web/test/export.test.ts
@@ -1,9 +1,14 @@
 import { WireRecord } from '../src/api/hdkCrud'
-import { internalExportProjectsData } from '../src/migrating/export'
-import { Profile } from '../src/types'
+import {
+  AllProjectsDataExport,
+  ProjectExportDataV1,
+  internalExportProjectsData,
+} from '../src/migrating/export'
+import { LayeringAlgorithm, Profile } from '../src/types'
 
 describe('test export functionality', () => {
   let whoami: WireRecord<Profile>
+  let mockProjectData: ProjectExportDataV1
 
   beforeEach(() => {
     whoami = {
@@ -21,25 +26,70 @@ describe('test export functionality', () => {
         isImported: false,
       },
     }
+
+    mockProjectData = {
+      projectMeta: {
+        creatorAgentPubKey: 'testAgentPubKey',
+        createdAt: 1234,
+        name: 'testProjectName',
+        image: 'testProjectImage',
+        passphrase: 'testPassphrase',
+        isImported: false,
+        layeringAlgorithm: LayeringAlgorithm.CoffmanGraham,
+        topPriorityOutcomes: [],
+        isMigrated: null,
+        actionHash: 'testProjectActionHash',
+      },
+      outcomes: {},
+      connections: {},
+      outcomeMembers: {},
+      outcomeComments: {},
+      entryPoints: {},
+      tags: {},
+    }
   })
+
   it('should return projects data', async () => {
-    const constructProjectDataFetchers = jest.fn()
-    const collectExportProjectData = jest.fn()
+    const constructProjectDataFetchers = jest.fn().mockReturnValue({
+      fetchProjectMeta: jest.fn(),
+      fetchEntryPoints: jest.fn(),
+      fetchOutcomeComments: jest.fn(),
+      fetchOutcomeMembers: jest.fn(),
+      fetchTags: jest.fn(),
+      fetchOutcomes: jest.fn(),
+      fetchConnections: jest.fn(),
+    })
+
+    const updateProjectMeta = jest.fn()
+
+    const collectExportProjectData = jest.fn().mockReturnValue(mockProjectData)
+
     const store = {
       dispatch: jest.fn(),
-      getState: jest.fn().mockReturnValue({ whoami }),
+      getState: jest.fn().mockReturnValue({
+        whoami,
+        cells: { profiles: null, projects: ['testProjectCellId'] },
+      }),
     }
+
     const toVersion = 'test'
     const onStep = jest.fn()
 
-    const result = await internalExportProjectsData(
+    const result: AllProjectsDataExport = await internalExportProjectsData(
       constructProjectDataFetchers,
       collectExportProjectData,
+      updateProjectMeta,
       store,
       toVersion,
       onStep
     )
 
-    expect(onStep).toHaveBeenCalled()
+    expect(result.myProfile).toEqual(whoami.entry)
+    expect(result.projects).toEqual([mockProjectData])
+
+    expect(constructProjectDataFetchers).toHaveBeenCalledTimes(1)
+    expect(collectExportProjectData).toHaveBeenCalledTimes(1)
+    expect(updateProjectMeta).toHaveBeenCalledTimes(1)
+    expect(onStep).toHaveBeenCalledTimes(1)
   })
 })
