e323413e73ecd506e8be84e9f130749754d939fa
Karl Bonnici <karl.bonnici100@gmail.com>
2023-07-28 09:19:38 -0700

    add assertions about what arguments should be passed to functional dependancies

--- web/test/import.test.ts
+++ web/test/import.test.ts
@@ -81,6 +81,7 @@ describe('importProjectsData()', () => {
           '132,45,36,67,75,209,140,160,204,62,71,45,229,66,99,63,6,255,250,52,234,238,45,50,174,198,118,29,208,28,207,156,147,252,58,99,131,165,51[:cell_id_divider:]132,32,36,97,138,27,24,136,8,80,164,189,194,243,82,224,72,205,215,225,2,27,126,146,190,40,102,187,244,75,191,172,155,196,247,226,220,92,1',
         ],
       },
+      agentAddress: 'testAgentAddress',
     }
     mockMigrationData = JSON.stringify(sampleGoodDataExport)
 
@@ -104,11 +105,26 @@ describe('importProjectsData()', () => {
     )
 
     expect(getAppWs).toHaveBeenCalledTimes(1)
+
     expect(createProfilesZomeApi).toHaveBeenCalledTimes(1)
+    expect(createProfilesZomeApi).toHaveBeenCalledWith({})
+
     expect(installProjectAppAndImport).toHaveBeenCalledTimes(1)
+    expect(installProjectAppAndImport).toHaveBeenCalledWith(
+      'testAgentAddress',
+      sampleGoodDataExport.projects[0],
+      sampleGoodDataExport.projects[0].projectMeta.passphrase,
+      store.dispatch
+    )
+
     expect(installProjectApp).toHaveBeenCalledTimes(1)
+    expect(installProjectApp).toHaveBeenCalledWith(
+      sampleGoodDataExport.projects[1].projectMeta.passphrase
+    )
+
     expect(store.dispatch).toHaveBeenCalled()
     expect(store.getState).toHaveBeenCalledTimes(1)
+
     expect(onStep).toHaveBeenCalledTimes(
       1 + sampleGoodDataExport.projects.length
     )
