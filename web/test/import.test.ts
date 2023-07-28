fd874e454ecf56fe763a4fffcff72c6066974569
Karl Bonnici <karl.bonnici100@gmail.com>
2023-07-28 16:18:33 -0700

    add unit test for installProjectAppAndImport()

--- web/test/import.test.ts
+++ web/test/import.test.ts
@@ -1,86 +1,149 @@
 import { CellId } from '@holochain/client'
 import ProfilesZomeApi from '../src/api/profilesApi'
 import { ProjectExportDataV1 } from '../src/migrating/export'
-import { internalImportProjectsData } from '../src/migrating/import'
-import { AgentPubKeyB64, CellIdString } from '../src/types/shared'
+import {
+  internalImportProjectsData,
+  internalInstallProjectAppAndImport,
+} from '../src/migrating/import'
+import {
+  Action,
+  AgentPubKeyB64,
+  CellIdString,
+  WithActionHash,
+} from '../src/types/shared'
 import { sampleGoodDataExport } from './sample-good-data-export'
+import ProjectsZomeApi from '../src/api/projectsApi'
+import { LayeringAlgorithm, ProjectMeta } from '../src/types'
+import { WireRecord } from '../src/api/hdkCrud'
+
+let store: any
+let getAppWs: any
+let createProfilesZomeApi: (appWebsocket: any) => ProfilesZomeApi
+let createProjectsZomeApi: (appWebsocket: any) => ProjectsZomeApi
+// let createWhoAmI: typeof ProfilesApi
+
+let installProjectAppAndImport: (
+  agentAddress: AgentPubKeyB64,
+  projectData: ProjectExportDataV1,
+  passphrase: string,
+  dispatch: any
+) => Promise<void>
+
+let installProjectApp: (
+  passphrase: string
+) => Promise<[CellIdString, CellId, string]>
+
+let importProjectData: (
+  projectData: ProjectExportDataV1,
+  projectsCellIdString: CellIdString,
+  dispatch: any
+) => Promise<{ [oldActionHash: string]: string }>
+
+let simpleCreateProjectMeta: (
+  cellId: CellIdString,
+  payload: WireRecord<ProjectMeta>
+) => Action<WireRecord<ProjectMeta>>
+
+let onStep: (completed: number, toComplete: number) => void
+
+let whoami: any
+let mockBaseRootState: any
+let mockGetState: any
+let mockCellIdString: string
+let mockAppWs: any
+let mockProjectMeta: any
+
+let mockMigrationData: any
+
+beforeEach(() => {
+  mockAppWs = {}
+  getAppWs = jest.fn().mockResolvedValue(mockAppWs)
+
+  createProfilesZomeApi = jest.fn().mockReturnValue({
+    profile: {
+      // turn this jest.fn into a var that you can track and assert on
+      createWhoami: jest.fn(),
+    },
+  })
+  mockProjectMeta = {
+    actionHash: 'testActionHash',
+    entryHash: 'testEntryHash',
+    entry: {
+      creatorAgentPubKey: 'testAgentPubKey',
+      createdAt: 1234,
+      name: 'testName',
+      image: null,
+      passphrase: 'testPassphrase',
+      isImported: false,
+      layeringAlgorithm: LayeringAlgorithm.CoffmanGraham,
+      topPriorityOutcomes: [],
+      isMigrated: null,
+    },
+    createdAt: 1234,
+    updatedAt: 1234,
+  }
+  createProjectsZomeApi = jest.fn().mockReturnValue({
+    projectMeta: {
+      simpleCreateProjectMeta: jest.fn().mockResolvedValue(mockProjectMeta),
+    },
+  })
+  installProjectAppAndImport = jest.fn()
+  mockCellIdString =
+    '132,45,36,204,129,221,8,19,206,244,229,30,210,95,157,234,241,47,13,85,105,207,55,138,160,87,204,162,244,122,186,195,125,254,5,185,165,224,66[:cell_id_divider:]132,32,36,97,138,27,24,136,8,80,164,189,194,243,82,224,72,205,215,225,2,27,126,146,190,40,102,187,244,75,191,172,155,196,247,226,220,92,1'
+
+  installProjectApp = jest
+    .fn()
+    .mockResolvedValue([mockCellIdString, ['abc'], 'testString'])
+
+  importProjectData = jest.fn()
+  simpleCreateProjectMeta = jest.fn()
+
+  onStep = jest.fn()
+
+  whoami = {
+    actionHash: 'testActionHash',
+    entryHash: 'testEntryHash',
+    createdAt: 1234,
+    updatedAt: 1234,
+    entry: {
+      firstName: 'testFirstName',
+      lastName: 'testLastName',
+      handle: 'testHandle',
+      status: 'Online',
+      avatarUrl: 'testAvatarUrl',
+      agentPubKey: 'testAgentPubKey',
+      isImported: false,
+    },
+  }
+
+  mockBaseRootState = {
+    whoami,
+    cells: {
+      profiles:
+        '132,45,36,204,129,221,8,19,206,244,229,30,210,95,157,234,241,47,13,85,105,207,55,138,160,87,204,162,244,122,186,195,125,254,5,185,165,224,66[:cell_id_divider:]132,32,36,97,138,27,24,136,8,80,164,189,194,243,82,224,72,205,215,225,2,27,126,146,190,40,102,187,244,75,191,172,155,196,247,226,220,92,1',
+      projects: [
+        '132,45,36,67,75,209,140,160,204,62,71,45,229,66,99,63,6,255,250,52,234,238,45,50,174,198,118,29,208,28,207,156,147,252,58,99,131,165,51[:cell_id_divider:]132,32,36,97,138,27,24,136,8,80,164,189,194,243,82,224,72,205,215,225,2,27,126,146,190,40,102,187,244,75,191,172,155,196,247,226,220,92,1',
+      ],
+    },
+    agentAddress: 'testAgentAddress',
+  }
+
+  mockMigrationData = JSON.stringify(sampleGoodDataExport)
+
+  mockGetState = jest.fn().mockReturnValue(mockBaseRootState)
+
+  store = {
+    dispatch: jest.fn(),
+    getState: mockGetState,
+  }
+})
 
 describe('importProjectsData()', () => {
-  let store: any
-  let getAppWs: any
-  let createProfilesZomeApi: (appWebsocket: any) => ProfilesZomeApi
-  // let createWhoAmI: typeof ProfilesApi
-  let installProjectAppAndImport: (
-    agentAddress: AgentPubKeyB64,
-    projectData: ProjectExportDataV1,
-    passphrase: string,
-    dispatch: any
-  ) => Promise<void>
-  let installProjectApp: (
-    passphrase: string
-  ) => Promise<[CellIdString, CellId, string]>
-  let onStep: (completed: number, toComplete: number) => void
-
-  let whoami: any
-  let mockBaseRootState: any
-  let mockGetState: any
-
-  let mockMigrationData: any
-
   // if you want a quick way to get at the type expressed in the signature
   // of a function you can use something similar to: ReturnType<typeof ProfilesApi>
   // aah nice, whats the ReturnType<> for?
   // it means if this is a function, what type does it return, e.g. Promise<SomeType>
   // gotcha. nice!
-
-  beforeEach(() => {
-    getAppWs = jest.fn().mockResolvedValue({})
-    createProfilesZomeApi = jest.fn().mockReturnValue({
-      profile: {
-        // turn this jest.fn into a var that you can track and assert on
-        createWhoami: jest.fn(),
-      },
-    })
-    installProjectAppAndImport = jest.fn()
-    installProjectApp = jest
-      .fn()
-      .mockResolvedValue(['testCellIdString', ['abc'], 'testString'])
-    onStep = jest.fn()
-    whoami = {
-      actionHash: 'testActionHash',
-      entryHash: 'testEntryHash',
-      createdAt: 1234, // nanoseconds
-      updatedAt: 1234, // nanoseconds
-      entry: {
-        firstName: 'testFirstName',
-        lastName: 'testLastName',
-        handle: 'testHandle',
-        status: 'Online',
-        avatarUrl: 'testAvatarUrl',
-        agentPubKey: 'testAgentPubKey',
-        isImported: false,
-      },
-    }
-    mockBaseRootState = {
-      whoami,
-      cells: {
-        profiles:
-          '132,45,36,204,129,221,8,19,206,244,229,30,210,95,157,234,241,47,13,85,105,207,55,138,160,87,204,162,244,122,186,195,125,254,5,185,165,224,66[:cell_id_divider:]132,32,36,97,138,27,24,136,8,80,164,189,194,243,82,224,72,205,215,225,2,27,126,146,190,40,102,187,244,75,191,172,155,196,247,226,220,92,1',
-        projects: [
-          '132,45,36,67,75,209,140,160,204,62,71,45,229,66,99,63,6,255,250,52,234,238,45,50,174,198,118,29,208,28,207,156,147,252,58,99,131,165,51[:cell_id_divider:]132,32,36,97,138,27,24,136,8,80,164,189,194,243,82,224,72,205,215,225,2,27,126,146,190,40,102,187,244,75,191,172,155,196,247,226,220,92,1',
-        ],
-      },
-      agentAddress: 'testAgentAddress',
-    }
-    mockMigrationData = JSON.stringify(sampleGoodDataExport)
-
-    mockGetState = jest.fn().mockReturnValue(mockBaseRootState)
-
-    store = {
-      dispatch: jest.fn(),
-      getState: mockGetState,
-    }
-  })
 
   it('should do something', async () => {
     await internalImportProjectsData(
@@ -161,5 +224,56 @@ describe('importProjectsData()', () => {
         "Cannot read properties of null (reading 'projects')"
       )
     }
+  })
+})
+
+describe('installProjectAppAndImport()', () => {
+  const agentAddress = 'testAgentAddress'
+  it('does something else', async () => {
+    await internalInstallProjectAppAndImport(
+      agentAddress,
+      sampleGoodDataExport.projects[0],
+      sampleGoodDataExport.projects[0].projectMeta.passphrase,
+      store.dispatch,
+      installProjectApp,
+      importProjectData,
+      getAppWs,
+      createProjectsZomeApi,
+      simpleCreateProjectMeta
+    )
+
+    expect(installProjectApp).toHaveBeenCalledTimes(1)
+    expect(installProjectApp).toHaveBeenCalledWith(
+      sampleGoodDataExport.projects[0].projectMeta.passphrase
+    )
+
+    expect(importProjectData).toHaveBeenCalledTimes(1)
+    expect(importProjectData).toHaveBeenCalledWith(
+      sampleGoodDataExport.projects[0],
+      mockCellIdString,
+      store.dispatch
+    )
+
+    expect(store.dispatch).toHaveBeenCalledTimes(2)
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: 'SET_MEMBER',
+      payload: {
+        cellIdString: mockCellIdString,
+        member: {
+          agentPubKey: 'testAgentAddress',
+        },
+      },
+    })
+
+    expect(getAppWs).toHaveBeenCalledTimes(1)
+
+    expect(createProjectsZomeApi).toHaveBeenCalledTimes(1)
+    expect(createProjectsZomeApi).toHaveBeenCalledWith(mockAppWs)
+
+    expect(simpleCreateProjectMeta).toHaveBeenCalledTimes(1)
+    expect(simpleCreateProjectMeta).toHaveBeenCalledWith(
+      mockCellIdString,
+      mockProjectMeta
+    )
   })
 })
