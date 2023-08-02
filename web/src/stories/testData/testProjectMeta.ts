ad683226d5b4aeb63668a87bf4269e10ba94ce96
Karl Bonnici <karl.bonnici100@gmail.com>
2023-08-02 09:13:11 -0700

    add type annotation to testProject

--- web/src/stories/testData/testProjectMeta.ts
+++ web/src/stories/testData/testProjectMeta.ts
@@ -1,19 +1,16 @@
-import { LayeringAlgorithm } from '../../types'
+import { LayeringAlgorithm, ProjectMeta } from '../../types'
+import { WithActionHash } from '../../types/shared'
 
-const testProject = {
-  projectMeta: {
-    testProjectCellId: {
-      creatorAgentPubKey: 'testCreatorAgentPubKey',
-      createdAt: 1234,
-      name: 'testProjectName',
-      image: null,
-      passphrase: 'testPassphrase',
-      isImported: false,
-      layeringAlgorithm: LayeringAlgorithm.CoffmanGraham,
-      topPriorityOutcomes: [],
-      isMigrated: null,
-    },
-  },
+const testProject: WithActionHash<ProjectMeta> = {
+  creatorAgentPubKey: 'testCreatorAgentPubKey',
+  createdAt: 1234,
+  name: 'testProjectName',
+  image: null,
+  passphrase: 'testPassphrase',
+  isImported: false,
+  layeringAlgorithm: LayeringAlgorithm.CoffmanGraham,
+  topPriorityOutcomes: [],
+  isMigrated: null,
   actionHash: 'testProjectActionHash',
 }
 
