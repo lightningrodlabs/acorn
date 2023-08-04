66f4ec553d4019f0ec68ed2d713ad2e12f85d2fd
Karl Bonnici <karl.bonnici100@gmail.com>
2023-08-04 08:45:11 -0700

    add mock actionHashMap for testing

---
+++ web/test/mockActionHashMap.ts
@@ -0,0 +1,18 @@
+const mockActionHashMap = {
+  tagActionHashMap: { oldTagActionHash: 'newTagActionHash' },
+  outcomeActionHashMap: { oldOutcomeActionHash: 'newOutcomeActionHash' },
+  connectionsActionHashMap: {
+    oldConnectionActionHash: 'newConnectionActionHash',
+  },
+  outcomeMembersActionHashMap: {
+    oldOutcomeMemberActionHash: 'newOutcomeMemberActionHash',
+  },
+  outcomeCommentActionHashMap: {
+    oldOutcomeCommentActionHash: 'newOutcomeCommentActionHash',
+  },
+  entryPointActionHashMap: {
+    oldEntryPointActionHash: 'newEntryPointActionHash',
+  },
+}
+
+export default mockActionHashMap
