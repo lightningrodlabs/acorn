5a1f511e6c12b26b43a0cc16f391b2f54df06761
Connor Turland <connorturland@gmail.com>
2023-08-16 12:06:28 -0700

    v11 is a winner

--- scripts/download-happs.sh
+++ scripts/download-happs.sh
@@ -2,5 +2,5 @@
 
 mkdir -p electron/binaries
 # bump this in tandem with bumping the INTEGRITY_VERSION_NUMBER
-wget https://github.com/lightningrodlabs/acorn-happ/releases/download/v9.0.0/profiles.happ -O electron/binaries/profiles.happ
-wget https://github.com/lightningrodlabs/acorn-happ/releases/download/v9.0.0/projects.happ -O electron/binaries/projects.happ
+wget https://github.com/lightningrodlabs/acorn-happ/releases/download/v11.0.0/profiles.happ -O electron/binaries/profiles.happ
+wget https://github.com/lightningrodlabs/acorn-happ/releases/download/v11.0.0/projects.happ -O electron/binaries/projects.happ
