86918269cf6bf07aa9a7d65cdb9ae6782c281b74
Karl Bonnici <karl.bonnici100@gmail.com>
2023-08-16 14:52:10 -0700

    add back classic layout algorithm using flextree

--- web/src/drawing/layoutFormula.ts
+++ web/src/drawing/layoutFormula.ts
@@ -17,6 +17,8 @@ import { sugiyama } from 'd3-dag'
 import { coffmanGraham } from 'd3-dag/dist/sugiyama/layering/coffman-graham'
 import { Graph } from '../redux/persistent/projects/outcomes/outcomesAsGraph'
 import { longestPath } from 'd3-dag/dist/sugiyama/layering/longest-path'
+import * as flextree from 'd3-flextree'
+const fl = flextree.flextree
 
 function checkOutcomeAgainstViewingFilters(
   outcome: ComputedOutcome,
@@ -76,6 +78,190 @@ function getBoundingRec(
 
 export { getBoundingRec }
 
+function layoutForTree(
+  tree: ComputedOutcome,
+  allOutcomeDimensions: DimensionsState,
+  projectCollapsedOutcomes: {
+    [outcomeActionHash: ActionHashB64]: boolean
+  },
+  hiddenSmalls: boolean,
+  hiddenAchieved: boolean
+): CoordinatesState {
+  const layout = fl().spacing((nodeA, nodeB) => {
+    return nodeA.path(nodeB).length * 40
+  })
+
+  const layoutTree = {}
+  // use recursion to add each outcome as a node in the graph
+  function addOutcome(outcome: ComputedOutcome, node: any, level: number) {
+    let numDescendants = 0
+    node.children = []
+    // Skip over the Outcome if it is included in the list
+    // of collapsed Outcomes
+    if (!projectCollapsedOutcomes[outcome.actionHash]) {
+      outcome.children.forEach((childOutcome) => {
+        const childNode = {}
+        // filter out children who don't match the viewing filter criteria
+        if (
+          checkOutcomeAgainstViewingFilters(
+            childOutcome,
+            hiddenSmalls,
+            hiddenAchieved
+          )
+        ) {
+          const descendantCount = addOutcome(childOutcome, childNode, level + 1)
+          node.children.push(childNode)
+          numDescendants += descendantCount + 1
+        }
+      })
+    }
+
+    const width = allOutcomeDimensions[outcome.actionHash].width
+    // to create the dynamic vertical height, we apply
+    // a power law to the number of descendants, with a heightened baseline
+    // because the number of descendants is a good measure of how wide this tree
+    // might be
+    const VERTICAL_SPACING = 100
+    const height =
+      allOutcomeDimensions[outcome.actionHash].height +
+      (numDescendants > 0 ? Math.pow(numDescendants + 25, 1.5) : 60) +
+      VERTICAL_SPACING
+
+    node.actionHash = outcome.actionHash
+    node.size = [width, height]
+
+    return numDescendants
+  }
+  // kick off the recursion
+  addOutcome(tree, layoutTree, 1)
+
+  const flTree = layout.hierarchy(layoutTree)
+  // run the layout algorithm, which will set an x and y property onto
+  // each node
+  layout(flTree)
+  // create a coordinates object
+  const coordinates = {}
+  // update the coordinates object
+  flTree.each((node) => {
+    // coordinates will represent the top left,
+    // but as-is they represent the center, so re-adjust them by half
+    // the width of the outcome
+    coordinates[node.data.actionHash] = {
+      x: node.x - node.size[0] / 2,
+      y: node.y,
+    }
+  })
+  return coordinates
+}
+
+const calculateCoordinatesForLayout = (
+  layeringAlgorithm: LayeringAlgorithm,
+  graph: Graph,
+  allOutcomeDimensions: DimensionsState,
+  collapsedOutcomes: {
+    [outcomeActionHash: string]: boolean
+  },
+  hiddenSmalls: boolean,
+  hiddenAchieved: boolean
+) => {
+  if (layeringAlgorithm === LayeringAlgorithm.Classic)
+    return calculateCoordinatesForClassic(
+      graph,
+      allOutcomeDimensions,
+      collapsedOutcomes,
+      hiddenSmalls,
+      hiddenAchieved
+    )
+  else
+    return layoutForGraph(
+      graph,
+      layeringAlgorithm,
+      allOutcomeDimensions,
+      collapsedOutcomes,
+      hiddenSmalls,
+      hiddenAchieved
+    )
+}
+
+function calculateCoordinatesForClassic(
+  graph: Graph,
+  allOutcomeDimensions: DimensionsState,
+  collapsedOutcomes: {
+    [outcomeActionHash: string]: boolean
+  },
+  hiddenSmalls: boolean,
+  hiddenAchieved: boolean
+): CoordinatesState {
+  // based on the dimensions, determine what the layout of each
+  // distinct tree will be
+  const layouts = graph.outcomes.computedOutcomesAsTree
+    .filter((computedOutcome) => {
+      // filter out trees which have been hidden
+      return checkOutcomeAgainstViewingFilters(
+        computedOutcome,
+        hiddenSmalls,
+        hiddenAchieved
+      )
+    })
+    .map((tree) => ({
+      outcome: tree,
+      layout: layoutForTree(
+        tree,
+        allOutcomeDimensions,
+        collapsedOutcomes,
+        hiddenSmalls,
+        hiddenAchieved
+      ),
+    }))
+  const HORIZONTAL_TREE_SPACING = 100
+  let coordinates: CoordinatesState = {}
+  // coordinates will be adjusted each time through this iteration
+  layouts.forEach((tree, index) => {
+    // in the case of the first one, let it stay where it is
+    if (index === 0) {
+      const adjusted: CoordinatesState = {}
+      const offsetFromZero = tree.layout[tree.outcome.actionHash].y
+      Object.keys(tree.layout).forEach((coordKey) => {
+        adjusted[coordKey] = {
+          x: tree.layout[coordKey].x,
+          y: tree.layout[coordKey].y - offsetFromZero,
+        }
+      })
+      coordinates = {
+        ...adjusted,
+      }
+    } else {
+      // in the case of all the rest, push it right, according to wherever the last one was positioned + spacing
+      const [_ltop, lastTreeRight, _lbottom, _lleft] = getBoundingRec(
+        layouts[index - 1].outcome,
+        coordinates,
+        allOutcomeDimensions
+      )
+      const [_ttop, _tright, _tbottom, thisTreeLeft] = getBoundingRec(
+        tree.outcome,
+        tree.layout,
+        allOutcomeDimensions
+      )
+      const adjusted: CoordinatesState = {}
+      const yOffset = -1 * tree.layout[tree.outcome.actionHash].y
+      const xOffset = lastTreeRight - thisTreeLeft + HORIZONTAL_TREE_SPACING
+      Object.keys(tree.layout).forEach((coordKey) => {
+        // adjust the coordinate by the xOffset and the yOffset
+        adjusted[coordKey] = {
+          x: xOffset + tree.layout[coordKey].x,
+          y: yOffset + tree.layout[coordKey].y,
+        }
+      })
+      coordinates = {
+        ...coordinates,
+        ...adjusted,
+      }
+    }
+  })
+
+  return coordinates
+}
+
 function layoutForGraph(
   graph: Graph,
   layeringAlgorithm: LayeringAlgorithm,
@@ -94,12 +280,9 @@ function layoutForGraph(
 
   const layeringAlgo = (layeringAlgo: LayeringAlgorithm) => {
     switch (layeringAlgo) {
-      // TODO: add back in when we figure out how to not have it crash
-      // case LayeringAlgorithm.Simplex:
-      // return simplex
-      case 'LongestPath':
+      case LayeringAlgorithm.LongestPath:
         return longestPath
-      case 'CoffmanGraham':
+      case LayeringAlgorithm.CoffmanGraham:
         return coffmanGraham
       default:
         return coffmanGraham
@@ -194,13 +377,12 @@ export default function layoutFormula(
   hiddenSmalls: boolean,
   hiddenAchieved: boolean
 ): LayoutState {
-  let coordinates = {}
   // just do this for efficiency, it's not going to
   // get displayed or rendered anywhere
   const ctx = document.createElement('canvas').getContext('2d')
 
   // determine what the dimensions of each outcome will be
-  const allOutcomeDimensions: {
+  const dimensions: {
     [actionHash: ActionHashB64]: { width: number; height: number }
   } = {}
 
@@ -215,17 +397,17 @@ export default function layoutFormula(
         width,
         projectTags,
       })
-      allOutcomeDimensions[outcomeActionHash] = {
+      dimensions[outcomeActionHash] = {
         width,
         height,
       }
     }
   )
 
-  coordinates = layoutForGraph(
-    graph,
+  const coordinates = calculateCoordinatesForLayout(
     layeringAlgorithm,
-    allOutcomeDimensions,
+    graph,
+    dimensions,
     collapsedOutcomes,
     hiddenSmalls,
     hiddenAchieved
@@ -233,6 +415,6 @@ export default function layoutFormula(
 
   return {
     coordinates,
-    dimensions: allOutcomeDimensions,
+    dimensions,
   }
 }
