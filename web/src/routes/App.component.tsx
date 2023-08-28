409aab05871d53cb1c0e7599bcb99eb9849d6f51
Connor Turland <connorturland@gmail.com>
2023-08-28 17:04:40 -0400

    don't do the entire update modal, just the bar

--- web/src/routes/App.component.tsx
+++ web/src/routes/App.component.tsx
@@ -150,7 +150,7 @@ const App: React.FC<AppProps> = ({
 
   useEffect(() => {
     if (updateVersionInfo) {
-      setShowUpdateModal(true)
+      setShowUpdateBar(true)
     }
   }, [JSON.stringify(updateVersionInfo)])
 
