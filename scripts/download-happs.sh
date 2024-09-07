5513bee93b61bc0156c3778a03f6781c8db68a31
Wesley Finck <wesleyfinck@gmail.com>
2024-09-07 14:59:24 -0700

    uncommend line that creates electron/binaries for release build

--- scripts/download-happs.sh
+++ scripts/download-happs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# mkdir -p electron/binaries
+mkdir -p electron/binaries
 # bump this in tandem with bumping the INTEGRITY_VERSION_NUMBER
 # -q is just "quiet" without the logs
 wget -q https://github.com/lightningrodlabs/acorn-happ/releases/download/v14.0.0/acorn.happ -O electron/binaries/acorn.happ
