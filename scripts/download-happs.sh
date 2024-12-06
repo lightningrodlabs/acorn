a741b7b9a81f980e4ec4c0a626f999b58b229091
Wesley Finck <wesleyfinck@gmail.com>
2024-12-06 15:52:43 -0800

    point to newer happ version

--- scripts/download-happs.sh
+++ scripts/download-happs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-mkdir -p electron/binaries
 # bump this in tandem with bumping the INTEGRITY_VERSION_NUMBER
 # -q is just "quiet" without the logs
-wget -q https://github.com/lightningrodlabs/acorn-happ/releases/download/v14.0.0/acorn.happ -O electron/binaries/acorn.happ
-wget -q https://github.com/lightningrodlabs/acorn-happ/releases/download/v14.0.0/projects.happ -O electron/binaries/projects.happ
+wget -q https://github.com/lightningrodlabs/acorn-happ/releases/download/v15.0.0/acorn.happ -O ./acorn.happ
+wget -q https://github.com/lightningrodlabs/acorn-happ/releases/download/v15.0.0/projects.happ -O ./projects.happ
