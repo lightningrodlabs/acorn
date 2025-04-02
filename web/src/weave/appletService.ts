02030d1c54a2a706a67dc26b3eee40f455995c17
Wesley Finck <wesleyfinck@gmail.com>
2025-04-02 16:23:57 -0700

    include svg icons

--- web/src/weave/appletService.ts
+++ web/src/weave/appletService.ts
@@ -8,7 +8,10 @@ import { CellIdWrapper } from '../domain/cellId'
 import { AcornState } from './acornState'
 import { ProjectMeta } from '../types'
 
-const appletServices: AppletServices = {
+const BOARD_ICON_SRC = `data:image/svg+xml;utf8,<svg width="800px" height="800px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="black" ><path d="M2.5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2h-11zm5 2h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zm-5 1a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm9-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"/></svg>`
+const CARD_ICON_SRC = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zm64 0v64h64V96H64zm384 0H192v64H448V96zM64 224v64h64V224H64zm384 0H192v64H448V224zM64 352v64h64V352H64zm384 0H192v64H448V352z"/></svg>`
+
+export const appletServices: AppletServices = {
   creatables: {
     board: {
       label: 'Project',
@@ -43,7 +46,7 @@ const appletServices: AppletServices = {
         throw new Error('No matching project meta found')
       }
       return {
-        icon_src: 'hierarchy.svg',
+        icon_src: BOARD_ICON_SRC,
         name: matchingProjectMeta.entry.name,
       }
     }
@@ -61,7 +64,7 @@ const appletServices: AppletServices = {
       throw new Error('No matching project meta found')
     }
     return {
-      icon_src: 'outcome.svg',
+      icon_src: CARD_ICON_SRC,
       name: `${projectMeta.entry.name}: ${matchingOutcome.outcome.entry.content}`,
     }
   },
@@ -104,7 +107,6 @@ const appletServices: AppletServices = {
       }
       matchingWals.push(hrl)
     }
-
-    return matchingWals
+    return Array.from(matchingWals)
   },
 }
